ac194cb59d180984f4c7f49160ba341c1a0273a7
chicha <chayanit.kunt@mail.kmutt.ac.th>
2025-04-28 01:16:22 +0700

    Update searchandstat.h

--- searchandstat.h
+++ searchandstat.h
@@ -4,7 +4,7 @@
 #include "task_management.h"
 
 void searchTasks(task* head, completedstack* stack, const char* keyword);
-void showStats(task* head, completedstack* stack);
+void showStats(task* head, completedstack* stack, date today);
 void doneToday(tasklist* list, completedstack* stack);
 
 #endif
