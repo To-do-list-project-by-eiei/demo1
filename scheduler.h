547bc2404b44526a6c6c80fb9c6ec03127ad67b8
chicha <chayanit.kunt@mail.kmutt.ac.th>
2025-04-29 00:31:45 +0700

    Update scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -24,9 +24,13 @@ date getToday();
 void setDueDate(task* t, int day, int month, int year);
 void simulateDayChange(task* head, date* currentDate);
 void adjustPriority(task* head, date today);
+void autoPriorityAdjust(task* head, date today);  // New function
+int getDaysBetween(date d1, date d2);  // New function
 void clearcompletedtask(stacknode** completedstack);
 void updateTaskStatuses(task* head, date today);
 int isDateSoon(date today, date duedate, int daysThreshold);
 
+// New function for reminders
+void checkReminders(task* head, date today);
 
 #endif // SCHEDULER_H
