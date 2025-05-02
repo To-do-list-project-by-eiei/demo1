89e99402ed480648c4503e56fbddf78339f63ffd
chicha <chayanit.kunt@mail.kmutt.ac.th>
2025-05-03 00:58:06 +0700

    Update searchandstat.h

--- searchandstat.h
+++ searchandstat.h
@@ -1,17 +1,17 @@
 #ifndef SEARCHANDSTAT_H
 #define SEARCHANDSTAT_H
 
-// Include task_management.h directly instead of forward declarations
+
 #include "task_management.h"
 
-// Function declarations
+
 void searchTasks(task* head, completedstack* stack, const char* keyword);
 void showStats(task* head, completedstack* stack, date today);
 void show_time_stats(task* head, completedstack* stack, date today, int period);
 void doneToday(tasklist* list, completedstack* stack);
 void printTaskInfo(task* t);
 
-// Add the combined stats function declaration here
+
 void show_combined_stats(task* head, completedstack* stack, date today);
 
-#endif // SEARCHANDSTAT_H
+#endif 
